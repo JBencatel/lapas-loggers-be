a0a98d5b6a764f364e700710685549ae2f7b05e7
Joana Bencatel <bencateljoana@gmail.com>
2020-06-27 15:21:37 +0100

    Tried to create everything necessary to create and view countries

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Country;
+use App\Http\Resources\Country as CountryResource;
+
+class CountryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $countries = Country::paginate(15);
+
+        return CountryResource::collection($countries);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// List countries
+Route::get('countries', 'CountryController@index');
+
+// Show single country
+Route::get('country/{id}', 'CountryController@show');
