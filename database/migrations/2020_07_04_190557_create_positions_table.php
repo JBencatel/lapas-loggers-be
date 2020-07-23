8abd644d962b827c09eae2cd8ee201ea373380ab
Joana Bencatel <bencateljoana@gmail.com>
2020-07-23 19:54:55 +0100

    MAde the slope and orientation fields nullable

--- database/migrations/2020_07_04_190557_create_positions_table.php
+++ database/migrations/2020_07_04_190557_create_positions_table.php
@@ -16,16 +16,16 @@ class CreatePositionsTable extends Migration
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
             $table->string('code');
-            $table->integer('slope');
-            $table->integer('orientation');
             $table->foreignId('shore_id')->constrained();
             $table->foreignId('shore_level_id')->constrained();
             $table->foreignId('shore_exposure_id')->constrained();
             $table->integer('shore_replicate');
             $table->string('shore_subreplicate', 1);
-            $table->string('microhabitat', 2);
+            $table->integer('slope')->nullable();
+            $table->integer('orientation')->nullable();
             $table->date('deployment_date')->nullable();
             $table->date('termination_date')->nullable();
+            $table->string('microhabitat', 2);
             $table->boolean('inactive');
             $table->integer('old_number')->nullable();
             $table->timestamps();
